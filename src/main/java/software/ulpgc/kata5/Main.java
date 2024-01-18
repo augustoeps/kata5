4a9d9ab6c7f6193fe36ef6193659d80c6039d48d
augusto <augustopaolone3@hotmail.com>
2024-01-18 14:35:25 +0100

    version 4

---
+++ src/main/java/software/ulpgc/kata5/Main.java
@@ -0,0 +1,34 @@
+package software.ulpgc.kata5;
+
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+
+public class Main {
+    public static void main(String[] args) {
+        Spark.port(8080);
+        Spark.get("/factorial", (req,res)->new CommandExecutor(req,res).execute(new FactorialCommand()));
+    }
+
+    private record CommandExecutor(Request request, Response response) {
+
+        public String execute(Command command) {
+                Command.Output output = command.execute(input());
+                response.status(output.code());
+                return output.result();
+            }
+
+            private Command.Input input() {
+                return new Command.Input() {
+                    @Override
+                    public String get(String key) {
+                        return oneOf(request.params(key), request.queryParams(key));
+                    }
+
+                    private String oneOf(String a, String b) {
+                        return a != null ? a : b;
+                    }
+                };
+            }
+        }
+}
